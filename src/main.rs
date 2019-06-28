853b5cc5a13c8e0ea37561a505cf5781471e4de2
readlnh <readlnh@163.com>
2019-06-28 23:07:12 +0800

    post03 finish

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,13 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    //vga_buffer::print_something();
-
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
